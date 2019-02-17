523fda862cdc95ba057c37b6b5960b99a80d57d2
Yura Vakulenko <vakulenkoyura211@gmail.com>
2019-02-17 09:44:18 +0200

    dz0, dz1 finished

---
+++ dz/dz0.php
@@ -0,0 +1,12 @@
+<?php
+
+const TEST_CONSTANTA = 'xyz';
+$userName = 'Yura Vakulenko';
+
+function showSomething() {
+	return 'SOMETHING GREAT RETURN';
+}
+
+if (1==1) {
+    echo 'hi, i am super '.$userName.'!';    
+}
